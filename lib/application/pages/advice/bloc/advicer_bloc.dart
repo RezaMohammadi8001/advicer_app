8f17ff0e68e324967e414cfbb8fb7f7e8e7c9ddb
Reza Mohammadi <rezamohammadi8001@gmail.com>
2024-01-04 20:39:30 +0330

    implement bloc for the advicer page

---
+++ lib/application/pages/advice/bloc/advicer_bloc.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+part 'advicer_event.dart';
+part 'advicer_state.dart';
+
+class AdvicerBloc extends Bloc<AdvicerEvent, AdvicerState> {
+  AdvicerBloc() : super(AdvicerInitial()) {
+    on<AdvicerEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
---
+++ lib/application/pages/advice/bloc/advicer_event.dart
@@ -0,0 +1,7 @@
+part of 'advicer_bloc.dart';
+
+@immutable
+abstract class AdvicerEvent {}
+class AdvicerRequestedEvent extends AdvicerEvent{
+  
+}
